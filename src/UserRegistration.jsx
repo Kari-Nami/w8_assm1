0df42624ff605c4ae0c4225ea52f4c2c59ec39d4
kari-nami <kazakova.katya@outlook.com>
2026-08-01 23:26:42 +0700

    inputs disabled after submitting

--- src/UserRegistration.jsx
+++ src/UserRegistration.jsx
@@ -136,15 +136,15 @@ function UserRegistration() {
       <div>
         <div className={"form-row"}>
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" name="username" value={userName} onChange={(event) => setUserName(event.target.value)}/>
+          <input disabled={isSubmitted} type="text" id="username" name="username" value={userName} onChange={(event) => setUserName(event.target.value)}/>
         </div>
         <div className={"form-row"}>
           <label htmlFor="firstname">Firstname</label>
-          <input type="text" id="firstname" name="firstname" value={firstName} onChange={(event) => setFirstName(event.target.value)}/>
+          <input disabled={isSubmitted} type="text" id="firstname" name="firstname" value={firstName} onChange={(event) => setFirstName(event.target.value)}/>
         </div>
         <div className={"form-row"}>
           <label htmlFor="lastname">Lastname</label>
-          <input type="text" id="lastname" name="lastname" value={lastName} onChange={(event) => setLastName(event.target.value)}/>
+          <input disabled={isSubmitted} type="text" id="lastname" name="lastname" value={lastName} onChange={(event) => setLastName(event.target.value)}/>
         </div>
         <div className={"form-row"}>
           <label htmlFor="gender-1">Gender</label>
@@ -152,7 +152,7 @@ function UserRegistration() {
             {genders.map((gender) => {
               return (
                 <div key={gender.value} className={"choice"}>
-                  <input type="radio" name="gender" id={gender.name} checked={userGender === gender.name} onChange={() => setUserGender(gender.name)}/>
+                  <input disabled={isSubmitted} type="radio" name="gender" id={gender.name} checked={userGender === gender.name} onChange={() => setUserGender(gender.name)}/>
                   <label htmlFor={gender.value}>{gender.name}</label>
                 </div>
               )
@@ -165,7 +165,7 @@ function UserRegistration() {
             {hobbies.map((hobby) => {
               return (
                 <div key={hobby.value} className={"choice"}>
-                  <input type="checkbox" name="hobby" id={hobby.name} checked={userHobbies.includes(hobby.name)} onChange={(event) => {
+                  <input disabled={isSubmitted} type="checkbox" name="hobby" id={hobby.name} checked={userHobbies.includes(hobby.name)} onChange={(event) => {
                       if (event.target.checked) { setUserHobbies([...userHobbies, hobby.name]) }
                       else { setUserHobbies(userHobbies.filter(chosenHobby => chosenHobby !== hobby.name)) }
                     }}/>
@@ -177,7 +177,7 @@ function UserRegistration() {
         </div>
         <div className={"form-row"}>
           <label htmlFor="department">Department</label>
-          <select id="department" name="department" value={selectedDepartment} onChange={(event) => setSelectedDepartment(event.target.value)}>
+          <select disabled={isSubmitted} id="department" name="department" value={selectedDepartment} onChange={(event) => setSelectedDepartment(event.target.value)}>
             {departments.map((department) => {
               return (
                   <option value={department.value} key={department.value}>{department.name}</option>
@@ -188,7 +188,7 @@ function UserRegistration() {
         </div>
         <div className={"form-row"}>
           <label htmlFor="position">Job Position</label>
-          <select id="position" name="position" value={job} onChange={(event) => setJob(event.target.value)}>
+          <select disabled={isSubmitted} id="position" name="position" value={job} onChange={(event) => setJob(event.target.value)}>
             {departments
                 .find((department) => {return department.value === selectedDepartment})?.positions
                 .map((position) => {
